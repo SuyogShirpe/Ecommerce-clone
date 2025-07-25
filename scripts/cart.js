3b44282312e41a6203b756f93a1786c10da011d0
Suyog Shirpe <shirpesuyog@gmail.com>
2025-07-25 14:24:53 +0530

    add working to cart's  delete button

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,11 +1,13 @@
-export const cart = [{
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6' ,
-    quantity:2 ,
-},
-{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
-}];
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
@@ -28,4 +30,14 @@ export function addToCart(productId) {
       quantity: quantity,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(productId);
+    }
+  });
+  cart = newCart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
-import { cart } from "/scripts/cart.js";
+import { cart,removeFromCart } from "/scripts/cart.js";
 import { products } from "../data/products.js";
+import {formatCurrency} from "./utils/money.js";
 
 let cartSummaryHTML='';
 
@@ -14,7 +15,7 @@ products.forEach((product)=>{
 });
 console.log(matchingProduct);
 
-   cartSummaryHTML += `<div class="cart-item-container">
+   cartSummaryHTML += `<div class="cart-item-container cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -28,7 +29,7 @@ console.log(matchingProduct);
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${(matchingProduct.priceCents / 100).toFixed(2)}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -37,7 +38,7 @@ console.log(matchingProduct);
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -50,7 +51,7 @@ console.log(matchingProduct);
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -63,7 +64,7 @@ console.log(matchingProduct);
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -76,7 +77,7 @@ console.log(matchingProduct);
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -94,3 +95,12 @@ console.log(matchingProduct);
 });
 
 document.querySelector(".order-summary").innerHTML=cartSummaryHTML;
+
+document.querySelectorAll(".delete-quantity-link").forEach((link) =>{
+  link.addEventListener('click', () =>{
+    const productId=link.dataset.productId;
+    removeFromCart(productId);
+
+    document.querySelector(`.cart-item-container-${productId}`).remove();
+  });
+});
